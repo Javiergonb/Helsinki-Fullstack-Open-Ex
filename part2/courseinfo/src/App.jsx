02b8fb5a9aeca6a9f3d3863380ab5ae95b77ced9
Javiergonb <javiergonb3@gmail.com>
2025-02-19 12:10:22 -0300

    part 2 ex 2.5 solution

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,30 +1,4 @@
-const Header = ({title}) => <h2>{title}</h2>
-
-const Content = ({parts}) => (
-  <div>
-    {parts.map(part => <Part key = {part.id} part = {part}></Part>)}
-  </div>
-)
-
-const Part = ({part}) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-)
-
-const Total = ({total}) => <p><b>Number of exercises {total}</b></p>
-
-const Course = ({course}) =>{
-  return (
-    <>
-      <Header title={course.name}/>
-      <Content parts = {course.parts}/>
-      <Total total={
-          course.parts.reduce((sum , part) => sum + part.exercises, 0)
-      }/>
-    </>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
