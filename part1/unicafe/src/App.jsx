4de21694b9ca914c52d2064e4368486af46bdcd9
Javiergonb <javiergonb3@gmail.com>
2025-02-18 22:26:45 -0300

    part 1 ex 1.6 solution

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,33 @@
 import { useState } from 'react'
 
+
+
+const FeedbackButton = ({text,onClick}) => <button onClick={onClick}>{text}</button>
+
+const Display = ({good,neutral,bad}) => {
+  return (
+    <>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+    </>
+  )
+}
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+
   return (
     <div>
-      code here
+      <h2>Give feedback</h2>
+      <FeedbackButton text={"good"} onClick={() => setGood(good + 1)}></FeedbackButton>
+      <FeedbackButton text={"neutral"} onClick={() => setNeutral(neutral + 1)}></FeedbackButton>
+      <FeedbackButton text={"bad"} onClick={() => setBad(bad + 1)}></FeedbackButton>
+      <h2>Statistics</h2>
+      <Display good={good} neutral={neutral} bad={bad}></Display>
     </div>
   )
 }
